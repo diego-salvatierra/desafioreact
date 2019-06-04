aad043933420d8f1d87b21e55b77d6cda8eabbca
Diego Salvatierra <diegosalva.ds@gmail.com>
2019-06-04 19:56:41 -0400

    Updates

---
+++ precache-manifest.33d226cc638b4df86e37676d453e536b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d73c473f491224ef68754a554ef1f0",
+    "url": "/desafioreact/index.html"
+  },
+  {
+    "revision": "1cc4924567f3cf51dfad",
+    "url": "/desafioreact/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "5a1a05f5274aae52602e",
+    "url": "/desafioreact/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1cc4924567f3cf51dfad",
+    "url": "/desafioreact/static/js/2.f9cb09fb.chunk.js"
+  },
+  {
+    "revision": "5a1a05f5274aae52602e",
+    "url": "/desafioreact/static/js/main.f9c425e7.chunk.js"
+  },
+  {
+    "revision": "3a5155ff5d279510783c",
+    "url": "/desafioreact/static/js/runtime~main.586bf084.js"
+  }
+]);
